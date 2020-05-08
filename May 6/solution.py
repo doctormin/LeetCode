a6de3ceddb9ceb45826cd461a1eb94c81bfa35e8
YiminZhao <doctormin@sjtu.edu.cn>
2020-05-08 16:49:30 +0800

    reformat

--- May 6/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
-
-if __name__ == "__main__":
-    x = Solution()
-    print(x.majorityElement([1,2,2,1,3,2]))
