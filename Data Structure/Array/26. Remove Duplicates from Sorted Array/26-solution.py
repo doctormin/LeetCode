1248b287efef80d96cd253fcc12374e0cf40a883
YiminZhao <doctormin@sjtu.edu.cn>
2020-06-29 00:17:15 +0800

    added : 26

--- Data Structure/Array/26. Remove Duplicates from Sorted Array/26-solution.py
+++ Data Structure/Array/26. Remove Duplicates from Sorted Array/26-solution.py
@@ -1,1 +1,6 @@
 #https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # in-place 
+        nums[:] = [nums[i] for i in range(0, len(nums)) if (i == len(nums)-1) or (nums[i] != nums[i+1])]
+        return len(nums)
